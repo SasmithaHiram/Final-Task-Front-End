c455d3a91043bdc9cbc4061ed2e9576b01579168
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-04-29 13:06:08 +0530

    Employee Model

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CustomerComponent } from './page/customer/customer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      {
+        path: 'dashboard',
+        component: DashboardComponent,
+        children: [
+          { path: 'customers', component: CustomerComponent },
+        ],
+      },
+];
---
+++ src/app/model/Employee.ts
@@ -0,0 +1,13 @@
+export class Customer {
+  id?: number;
+  name: string;
+  department: string;
+  email: string;
+
+  constructor(name: string, department: string, email: string, id?: number) {
+    this.id = id;
+    this.name = name;
+    this.department = department;
+    this.email = email;
+  }
+}
